d1c796c66bdf9ee895f28fc1fa97c06215dbcd4a
Lizz <lizwesterband@gmail.com>
2020-09-03 15:38:54 -0700

    [components] Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import Welcome from "./components/Welcome"
 import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
 import About from "./components/About"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Welcome />
       <Footer />
+
     </div>
   );
 }
