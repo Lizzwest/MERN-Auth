3a29686324742fe57683361da27af25f66291477
Lizz <lizwesterband@gmail.com>
2020-09-03 15:08:17 -0700

    [components] Add about and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import Welcome from "./components/Welcome"
+import Footer from "./components/Footer"
+import About from "./components/About"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>MERN Auth Frontend</h1>
+      <Welcome />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const About = () =>{
+    return(
+    <div>
+        <h1>This is all about me</h1>
+        <p> Stinking bishop stilton parmesan. Cheddar fondue melted cheese squirty cheese 
+            red leicester who moved my cheese cheesy grin fromage frais. Stinking bishop when
+             the cheese comes out everybody's happy macaroni cheese squirty cheese rubber cheese 
+             cut the cheese feta dolcelatte. Cheeseburger port-salut cheese slices rubber cheese 
+             cheese slices queso.
+
+            Gouda manchego manchego. Fromage frais bocconcini rubber cheese emmental cheesy grin 
+            lancashire cheese triangles everyone loves. Rubber cheese parmesan fromage frais monterey 
+            jack fondue when the cheese comes out everybody's happy macaroni cheese pecorino. Cheeseburger.
+
+            Lancashire cheese and wine halloumi. Bocconcini cow caerphilly rubber cheese queso melted 
+            cheese monterey jack pepper jack. Everyone loves boursin taleggio swiss bavarian bergkase 
+            queso caerphilly the big cheese. Cheese triangles melted cheese port-salut boursin croque 
+            monsieur cheese and wine babybel cheese strings. Cheese strings taleggio cow everyone loves.</p>
+    </div>
+    )}
+    export default About
