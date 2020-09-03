1da5e00b760d733b8321513a2ca148f58316b00f
Lizz <lizwesterband@gmail.com>
2020-09-03 09:29:09 -0700

    [backend] Add backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const express = require("express")
+const app = express();
+const cors = require("cors")
+const port = process.env.PORT || 8000
+const passport = require("passport")
+
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.status(200).json({message: " Smile, you're being watched from the backend👀"})
+    // res.send("Backend home route")
+})
+
+app.listen(port, ()=>{
+    console.log("I couldnt see what rome typed in time")
+})
